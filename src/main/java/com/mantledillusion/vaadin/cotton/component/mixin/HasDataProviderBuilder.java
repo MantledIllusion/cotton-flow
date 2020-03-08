3283b0ce4fe51aff6f190a68c336583dcedabf6b
Tobias Weber <weber.tobias@live.de>
2020-03-08 19:35:50 +0100

    Added MappedConfigurableFilter for convenience

--- src/main/java/com/mantledillusion/vaadin/cotton/component/mixin/HasDataProviderBuilder.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/component/mixin/HasDataProviderBuilder.java
@@ -14,7 +14,9 @@ import com.vaadin.flow.function.SerializablePredicate;
 import com.vaadin.flow.shared.Registration;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
 /**
@@ -91,6 +93,48 @@ public interface HasDataProviderBuilder<C extends HasDataProvider<E>, E, F exten
          */
         protected void notifyConfigurationChanged() {
             this.listeners.forEach(ConfigurationChangedListener::configurationChanged);
+        }
+    }
+
+    /**
+     * A basic {@link ConfigurableFilter} that allows saving and retrieving a filter configuration using a {@link Map}.
+     *
+     * @param <E> The element type.
+     */
+    class MappedConfigurableFilter<E> extends BasicConfigurableFilter<E> {
+
+        private final Map<String, Object> filter = new HashMap<>();
+
+        /**
+         * Returns if the value of the given key is set.
+         *
+         * @param key The key; might be null.
+         * @return True if the value is set, false otherwise
+         */
+        public boolean contains(String key) {
+            return this.filter.containsKey(key);
+        }
+
+        /**
+         * Returns the value to the given key.
+         *
+         * @param <V> The key's value type.
+         * @param key The key whose value to retrieve; might be null.
+         * @return The value, might be null if the key is not set or set to null
+         */
+        public <V> V get(String key) {
+            return (V) this.filter.get(key);
+        }
+
+        /**
+         * Sets the value to the given key.
+         *
+         * @param <V> The key's value type.
+         * @param key The key whose value to set; might be null.
+         * @param value The value to set; might be null.
+         */
+        public <V> void set(String key, V value) {
+            this.filter.put(key, value);
         }
     }
 
