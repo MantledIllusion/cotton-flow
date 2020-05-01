93761ebdef70373adba7fa8035cd2e845bbaf900
Tobias Weber <weber.tobias@live.de>
2020-05-01 13:59:39 +0200

    Pumped up TreeGridBuilder to have the same functionality as GridBuilder

--- src/main/java/com/mantledillusion/vaadin/cotton/component/builders/TreeGridBuilder.java
+++ src/main/java/com/mantledillusion/vaadin/cotton/component/builders/TreeGridBuilder.java
@@ -1,32 +1,26 @@
 package com.mantledillusion.vaadin.cotton.component.builders;
 
 import com.mantledillusion.vaadin.cotton.component.ComponentBuilder;
-import com.mantledillusion.vaadin.cotton.component.mixin.*;
-import com.vaadin.flow.component.grid.GridVariant;
+import com.mantledillusion.vaadin.cotton.component.mixin.HasDataProviderBuilder;
 import com.vaadin.flow.component.treegrid.TreeGrid;
+import com.vaadin.flow.function.ValueProvider;
 
 /**
  * {@link ComponentBuilder} for {@link TreeGrid}s.
+ *
+ * @param <E> The element type of the {@link TreeGrid}
+ * @param <F> The filter type of the {@link TreeGrid}
  */
-public class TreeGridBuilder<E> extends AbstractComponentBuilder<TreeGrid<E>, TreeGridBuilder<E>> implements
-        HasElementBuilder<TreeGrid<E>, TreeGridBuilder<E>>,
-        HasSizeBuilder<TreeGrid<E>, TreeGridBuilder<E>>,
-        HasThemeVariantBuilder<TreeGrid<E>, TreeGridBuilder<E>, GridVariant>,
-        HasStyleBuilder<TreeGrid<E>, TreeGridBuilder<E>>,
-        HasEnabledBuilder<TreeGrid<E>, TreeGridBuilder<E>>,
-        HasItemsBuilder<TreeGrid<E>, E, TreeGridBuilder<E>>,
-        HasHierarchicalDataProviderBuilder<TreeGrid<E>, E, TreeGridBuilder<E>> {
+public class TreeGridBuilder<E, F extends HasDataProviderBuilder.ConfigurableFilter<E>> extends AbstractGridBuilder<TreeGrid<E>, TreeGridBuilder<E, F>, E, F> {
 
-    private TreeGridBuilder() {
-
-    }
+    private TreeGridBuilder() {}
 
     /**
      * Factory method for a new instance.
      *
      * @return A new instance, never null.
      */
-    public static TreeGridBuilder<Object> create() {
+    public static TreeGridBuilder<Object, ConfigurableFilter<Object>> create() {
         return new TreeGridBuilder<>();
     }
 
@@ -37,7 +31,21 @@ public class TreeGridBuilder<E> extends AbstractComponentBuilder<TreeGrid<E>, Tr
      * @param elementType The class type of the element; might be null.
      * @return A new instance, never null.
      */
-    public static <E> TreeGridBuilder<E> create(Class<E> elementType) {
+    public static <E> TreeGridBuilder<E, ConfigurableFilter<E>> create(Class<E> elementType) {
+        return new TreeGridBuilder<>();
+    }
+
+    /**
+     * Factory method for a new instance.
+     *
+     * @param <E> The element type.
+     * @param <F> The filter type.
+     * @param elementType The class type of the element; might be null.
+     * @param filterType The class type of the filter; might be null.
+     * @return A new instance, never null.
+     */
+    public static <E, F extends ConfigurableFilter<E>> TreeGridBuilder<E, F> create(Class<E> elementType,
+                                                                                    Class<F> filterType) {
         return new TreeGridBuilder<>();
     }
 
@@ -46,8 +54,17 @@ public class TreeGridBuilder<E> extends AbstractComponentBuilder<TreeGrid<E>, Tr
         return new TreeGrid<>();
     }
 
-    @Override
-    public String toVariantName(GridVariant variant) {
-        return variant.getVariantName();
+    /**
+     * Builder method, configures a new column.
+     *
+     * @see TreeGrid#addHierarchyColumn(ValueProvider)
+     * @param valueProvider
+     *            The value provider; might <b>not</b> be null.
+     * @return A new {@link GridColumnBuilder}, never null
+     */
+    public GridColumnBuilder configureHierarchyColumn(ValueProvider<E, ?> valueProvider) {
+        GridColumnBuilder columnBuilder = new GridColumnBuilder(grid -> grid.addHierarchyColumn(valueProvider));
+        configure(columnBuilder);
+        return columnBuilder;
     }
 }
